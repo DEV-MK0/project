a86fbd4b41ac414aa0217ae27d5e74ab2dc24e9a
DEV-MK0 <markhess098@gmail.com>
2026-03-21 01:41:18 +0100

    icon added

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 SCHALTmin = 5.0
